8ea1c9ffa4457ec1537961d47c5f6c938d057adc
Tobias Ryttlinger <tosryr@gmail.com>
2020-02-03 08:37:10 +0100

    changing stuff

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,13 @@
 
 
-$(document).ready(function(){
-	$('#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
 });
+	 document.getElementById("nav-icon4").onclick = function () {
+		 if (document.URL == "file:///C:/Users/Tobias/Documents/GitHub/TobiasRyttlinger.github.io/Index.html" ) {
+ 		 			location.href = "Menu.html";
+				}
+	 			else{
+					 location.href = "Index.html";
+				}
+	 	 };
