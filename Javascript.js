4987b4e3ff6c4f80ea76fcaba1c328d0f2a92f30
Tobias Ryttlinger <tosryr@gmail.com>
2020-02-05 15:30:55 +0100

    added stuff

--- Javascript.js
+++ Javascript.js
@@ -1,13 +1,7 @@
 
 
-window.addEventListener("beforeunload", function () {
-  document.body.classList.add("animate-out");
+$(document).ready(function(){
+	$('#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
 });
-	 document.getElementById("nav-icon4").onclick = function () {
-		 if (document.URL == "file:///C:/Users/Tobias/Documents/GitHub/TobiasRyttlinger.github.io/Index.html" ) {
- 		 			location.href = "Menu.html";
-				}
-	 			else{
-					 location.href = "Index.html";
-				}
-	 	 };
